6ec16dc4670ae1218f8d5467557cf5192749a2f8
Dugar ERKHEMBAYAR <dugarerkhembayar@Dugars-MacBook-Pro.local>
2023-11-30 00:55:22 +0800

    Finished login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,25 +3,35 @@ import './login.css'
 import Elf_Logo from '../../elf_logo.png'
 
 
+
 const LoginSignup = () => {
     return (
         <div className='container'>
+            
             <div className='header'>
-                <img src={Elf_Logo} />
+                <img src={Elf_Logo} alt="" />
                 <div className='underline'></div>
-                </div> 
-            <div className='inputs'>
-                <div className='input'>
-                    <input type="email" placeholder='email хаяг' />
-            </div>
-                <div className='input'>
-                    <input type="password" placeholder='Нууц үг' />
-            </div>
-            </div>
-            <div className="forgot-password">Lost Passwor? <span>Click here</span></div>
-            <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+            </div> 
+            <div className= "login">
+                <div className='inputs'>
+                    <div className='input'>
+                        <input type="email" placeholder='email хаяг' />
+                    </div>
+                    <div className='input'>
+                        <input type="password" placeholder='Нууц үг' />
+                    </div>
+                </div>
+                <div className="bottom-text">
+                    <div>
+                    <input type='checkbox' id='check' />
+                    <label for='check'>Сануулах</label>
+                    </div>
+                    
+                    <div className="forgot-password">Нууц үгээ мартсан уу? </div>
+                </div>
+                <div className="submit-container">
+                        <div className="submit">Нэвтрэх</div>
+                    </div>
             </div>
         </div>
     )
